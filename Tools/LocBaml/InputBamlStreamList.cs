cd3dd9baf713cf6cb77b4219edec36c2d787c86e
mtaIlma <156999258+mtaIlma@users.noreply.github.com>
2025-08-07 16:02:41 +0200

    Update InputBamlStreamList.cs

--- Tools/LocBaml/InputBamlStreamList.cs
+++ Tools/LocBaml/InputBamlStreamList.cs
@@ -44,6 +44,12 @@ namespace BamlLocalization
                 }
                 case FileType.RESOURCES:
                 {
+                    using (ResourceReader resourceReader4 = new ResourceReader(options.Input))
+                    {
+                        // enumerate all bamls in a resources
+                        EnumerateBamlInResources(resourceReader4, options.Input);
+                    }
+                    
                     using (ResourceReader resourceReader3 = new ResourceReader(options.Input))
                     {
                         // enumerate all bamls in a resources
